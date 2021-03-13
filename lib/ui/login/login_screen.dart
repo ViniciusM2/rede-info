2337986a0663a65b725d39726ef550d281f4e30c
Vinícius Menezes Monte <viniciusmonte2010@gmail.com>
2021-03-13 16:46:16 -0300

    wide layout implemented for sign in and sign up screens

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -1,59 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:redeinfo/ui/widgets/info_logo.dart';
 import 'package:redeinfo/ui/login/widgets/login_form.dart';
+import 'package:get/get.dart';
 
 class LoginScreen extends StatelessWidget {
+  final _height = Get.height.obs;
+  get height => this._height.value;
+  set height(value) => this._height.value = value;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ScrollConfiguration(
-        behavior: _MyScrollBehavior(),
-        child: Container(
-          child: Center(
-            child: OrientationBuilder(builder: (context, orientation) {
-              if (orientation == Orientation.portrait) {
-                return SingleChildScrollView(
-                  child: Row(
-                    children: [
-                      Spacer(),
-                      Expanded(
-                        flex: 8,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            InfoLogo(),
-                            LoginForm(),
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                    ],
-                  ),
-                );
-              } else {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Flexible(
-                        flex: 3,
-                        child: InfoLogo(),
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Flexible(
-                        flex: 2,
-                        child: SingleChildScrollView(
-                          child: LoginForm(),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }
-            }),
+      body: SafeArea(
+        child: ScrollConfiguration(
+          behavior: _MyScrollBehavior(),
+          child: Container(
+            child: Center(
+              child: OrientationBuilder(builder: (context, orientation) {
+                height = Get.height;
+                if (orientation == Orientation.portrait) {
+                  return _VerticalLayout();
+                } else {
+                  if (context.height >= 600)
+                    return _WideLayout();
+                  else
+                    return _HorizontalLayout();
+                }
+              }),
+            ),
           ),
         ),
       ),
@@ -66,5 +40,79 @@ class _MyScrollBehavior extends ScrollBehavior {
   Widget buildViewportChrome(
       BuildContext context, Widget child, AxisDirection axisDirection) {
     return child;
+  }
+}
+
+class _HorizontalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Flexible(
+            flex: 3,
+            child: InfoLogo(),
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Flexible(
+            flex: 2,
+            child: SingleChildScrollView(
+              child: LoginForm(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _VerticalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Row(
+        children: [
+          Spacer(),
+          Expanded(
+            flex: 8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InfoLogo(),
+                LoginForm(),
+              ],
+            ),
+          ),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
+
+class _WideLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Row(
+        children: [
+          Spacer(),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InfoLogo(),
+                LoginForm(),
+              ],
+            ),
+          ),
+          Spacer(),
+        ],
+      ),
+    );
   }
 }
--- lib/ui/sign_up/sign_up_screen.dart
+++ lib/ui/sign_up/sign_up_screen.dart
@@ -1,60 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:get/get.dart';
 import 'package:redeinfo/ui/sign_up/widgets/sign_up_form.dart';
 import 'package:redeinfo/ui/widgets/info_logo.dart';
-
-import 'widgets/link_to_login_screen.dart';
+import 'package:get/get.dart';
 
 class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: OrientationBuilder(
-            builder: (context, orientation) {
-              if (orientation == Orientation.portrait) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Spacer(),
-                      Expanded(
-                        flex: 8,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            InfoLogo(),
-                            SingleChildScrollView(
-                              child: SignUpForm(),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                    ],
-                  ),
-                );
-              } else {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(flex: 3, child: InfoLogo()),
-                      SizedBox(width: 15,),
-                      Flexible(
-                        flex: 2,
-                        child: SingleChildScrollView(
-                          child: SignUpForm(),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }
-            },
+        child: ScrollConfiguration(
+          behavior: _MyScrollBehavior(),
+          child: Center(
+            child: OrientationBuilder(
+              builder: (context, orientation) {
+                if (orientation == Orientation.portrait) {
+                  return _VerticalLayout();
+                } else {
+                  if (context.height >= 600)
+                    return _WideLayout();
+                  else
+                    return _HorizontalLayout();
+                }
+              },
+            ),
           ),
         ),
       ),
@@ -67,5 +35,82 @@ class _MyScrollBehavior extends ScrollBehavior {
   Widget buildViewportChrome(
       BuildContext context, Widget child, AxisDirection axisDirection) {
     return child;
+  }
+}
+
+class _HorizontalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(flex: 3, child: InfoLogo()),
+          SizedBox(
+            width: 15,
+          ),
+          Flexible(
+            flex: 2,
+            child: SingleChildScrollView(
+              child: SignUpForm(),
+            ),
+          ),
+          // Flexible(child: Text('${context.height}'))
+        ],
+      ),
+    );
+  }
+}
+
+class _VerticalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Spacer(),
+          Expanded(
+            flex: 8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InfoLogo(),
+                SingleChildScrollView(
+                  child: SignUpForm(),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
+
+class _WideLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Row(
+        children: [
+          Spacer(),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InfoLogo(),
+                SingleChildScrollView(
+                  child: SignUpForm(),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+        ],
+      ),
+    );
   }
 }
